5e9adbcbd4087a62af5e3dc63ecb9e9fe6b04bc8
Yog <a1661705892@outlook.com>
2022-08-23 19:32:05 +0800

    1

---
+++ src/main/java/com/example/springbootexample/controller/find.java
@@ -0,0 +1,8 @@
+package com.example.springbootexample.controller;
+
+import com.example.springbootexample.domain.User;
+import com.example.springbootexample.repository.FindUser;
+
+public abstract class find implements FindUser {
+
+}
---
+++ src/main/java/com/example/springbootexample/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.springbootexample.service;
+
+import java.util.List;
+
+import com.example.springbootexample.domain.User;
+
+public interface UserService {
+    User loginService(String uname, String password);
+
+    User registService(User user);
+
+    List<User> findService(Long uid);
+}
